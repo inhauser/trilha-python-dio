00c0fb5895a7045b1f6d690048d9f3406816ef80
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-27 19:46:16 -0300

    Update 11_extend.py

--- 01 - Estrutura de dados/01 - Listas/11_extend.py
+++ 01 - Estrutura de dados/01 - Listas/11_extend.py
@@ -1,3 +1,5 @@
+# ao invés de utilizar o append, que permite a adição de apenas um item à lista, utilizo extend para adicionar diversos itens.
+
 linguagens = ["python", "js", "c"]
 
 print(linguagens)  # ["python", "js", "c"]
