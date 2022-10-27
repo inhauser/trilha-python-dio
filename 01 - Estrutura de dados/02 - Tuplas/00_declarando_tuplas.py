d10f9168519fdbf300065aeb7bedc9644392fa36
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-27 20:21:12 -0300

    Update 00_declarando_tuplas.py

--- 01 - Estrutura de dados/02 - Tuplas/00_declarando_tuplas.py
+++ 01 - Estrutura de dados/02 - Tuplas/00_declarando_tuplas.py
@@ -29,3 +29,14 @@ matriz[0] # (1, "a", 2)
 matriz[0][0] # 1
 matriz[0][-1] # 2
 matriz[-1][-1] # "c"
+
+# fatiamento / slicing
+
+tupla = ("p", "y", "t", "h", "o", "n",)
+
+tupla[2:] # ("t", "h", "o", "n")
+tupla[:2] # ("p", "y")
+tupla[1:3] # ("y", "t")
+tupla[0:3:2] # ("p", "t")
+tupla[::] # ("p", "y", "t", "h", "o", "n")
+tupla[::-1] # ("n", "o", "h", "t", "y", "p")
