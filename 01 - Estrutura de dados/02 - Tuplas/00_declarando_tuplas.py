7d98366e624f9ab955cd05acebeb24c7d270af42
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-27 20:15:20 -0300

    Update 00_declarando_tuplas.py

--- 01 - Estrutura de dados/02 - Tuplas/00_declarando_tuplas.py
+++ 01 - Estrutura de dados/02 - Tuplas/00_declarando_tuplas.py
@@ -1,3 +1,8 @@
+# Tuplas: imutáveis, bem mais restritas. Listas: mutáveis.
+# É de boa prática colocar vírgula no final. Isso evita que o python confunda a tupla com uma ordem de precedência entre parênteses (se não for utilizado tuple).
+# uma tupla pode conter uma lista dentro dela, como no exemplo numérico abaixo. Todos os tipos de objeto, inclusive tuplas aninhadas.
+# utiliza-se o colchete e o valor do índice, da mesma forma que se faz na lista, inclusive os negativos - slice
+
 frutas = (
     "laranja",
     "pera",
@@ -13,3 +18,14 @@ print(numeros)
 
 pais = ("Brasil",)
 print(pais)
+
+matriz = (
+    (1, "a", 2),
+    ("b", 3, 4),
+    (6, 5, "c"),
+)
+
+matriz[0] # (1, "a", 2)
+matriz[0][0] # 1
+matriz[0][-1] # 2
+matriz[-1][-1] # "c"
