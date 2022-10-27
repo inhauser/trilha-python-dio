a986f61ed07c9248dd0d8ea960e290ddb0afaa30
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-27 19:44:18 -0300

    Update 09_copy.py

--- 01 - Estrutura de dados/01 - Listas/09_copy.py
+++ 01 - Estrutura de dados/01 - Listas/09_copy.py
@@ -1,5 +1,18 @@
+# como a lista é mutável, se eu copiar a lista, ela pode ser mantida na lista originária, ou na lista copiada, permitindo alterações dissociadas.
+# o id permite identificar que se tratam de listas diferentes
+
 lista = [1, "Python", [40, 30, 20]]
 
 lista.copy()
 
 print(lista)  # [1, "Python", [40, 30, 20]]
+
+l2 = lista.copy()
+
+print(id(l2), id(lista))
+
+l2[0] = 2
+
+print(l2)
+
+print(lista)
