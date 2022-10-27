045ac8503293a26387fa69fde6a73e15ca56d5ea
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-27 20:00:03 -0300

    Update 18_sorted.py

--- 01 - Estrutura de dados/01 - Listas/18_sorted.py
+++ 01 - Estrutura de dados/01 - Listas/18_sorted.py
@@ -1,4 +1,9 @@
+# built-in sorte, função.
+# Funciona como o sort, porém é uma função e a sintaxe é específica, diferindo daquela do sort.
+
 linguagens = ["python", "js", "c", "java", "csharp"]
 
 print(sorted(linguagens, key=lambda x: len(x)))  # ["c", "js", "java", "python", "csharp"]
 print(sorted(linguagens, key=lambda x: len(x), reverse=True))  # ["python", "csharp", "java", "js", "c"]
+
+print(sorted(linguagens))
