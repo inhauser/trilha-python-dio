1a4c062d35024a5d4f183af2c25918eba425fe32
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-27 19:52:40 -0300

    Update 14_remove.py

--- 01 - Estrutura de dados/01 - Listas/14_remove.py
+++ 01 - Estrutura de dados/01 - Listas/14_remove.py
@@ -1,3 +1,6 @@
+# diferente do pop que remove com base no índice da pilha, o remove atua sobre o objeto descrito em si.
+# se a ocorrência é repetida, ele somente remove na ação um item por vez.
+
 linguagens = ["python", "js", "c", "java", "csharp"]
 
 linguagens.remove("c")
