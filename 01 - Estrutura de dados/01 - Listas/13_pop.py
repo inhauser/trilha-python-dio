c047607a7556574b6cc0018b527e817a91a07f41
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-27 19:51:16 -0300

    Update 13_pop.py

--- 01 - Estrutura de dados/01 - Listas/13_pop.py
+++ 01 - Estrutura de dados/01 - Listas/13_pop.py
@@ -1,3 +1,6 @@
+# lista é organizada como pilha. como pilha de pratos, 1, 2, 3. Sem alterar a pilha removeria primeiro o 3 e assim por diante.
+# o pop retira o último item da lista, ou posso indicar o índice do objeto que quero remover.
+
 linguagens = ["python", "js", "c", "java", "csharp"]
 
 print(linguagens.pop())  # csharp
