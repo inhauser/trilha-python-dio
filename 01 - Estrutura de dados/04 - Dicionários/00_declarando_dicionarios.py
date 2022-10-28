eed35895f38467ef8c8d5c029a2aee242caa621a
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-28 07:22:27 -0300

    Update 00_declarando_dicionarios.py

--- 01 - Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
+++ 01 - Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
@@ -1,3 +1,6 @@
+# chave:valor - chave tem ques er imutável e pode ser, por ex, tupla. valor pode ser qquer tipo de objeto. Chave valor único. Dict (:) ou {:}
+# para recuperar valor, faço a chamada plea chave.
+
 pessoa = {"nome": "Guilherme", "idade": 28}
 print(pessoa)
 
