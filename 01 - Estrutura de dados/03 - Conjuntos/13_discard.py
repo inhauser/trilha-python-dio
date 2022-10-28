38cc87e8839e7992ec3fb13067c5ca69bc2cc1f9
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-27 21:25:09 -0300

    Update 13_discard.py

--- 01 - Estrutura de dados/03 - Conjuntos/13_discard.py
+++ 01 - Estrutura de dados/03 - Conjuntos/13_discard.py
@@ -1,3 +1,5 @@
+# se passar um valor que existe, ele descarta. se o valor não existe, ele não faz nada, e não retorna erro.
+
 numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
 
 print(numeros)  # {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
