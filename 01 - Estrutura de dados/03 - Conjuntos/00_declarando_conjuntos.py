2d6dd27dbca4ba0a23a4a3d22ee5f59d2a428362
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-27 21:14:07 -0300

    Update 00_declarando_conjuntos.py

--- 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
+++ 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
@@ -1,3 +1,7 @@
+# elimina os objetos duplicados dentro de um elemento iterável.
+# set não garante a ordem, não confiar na ordem, pode não estar ordenado.
+# ao invés de escrever set posso substituir por chaves {}
+
 numeros = set([1, 2, 3, 1, 3, 4])
 print(numeros)  # {1, 2, 3, 4}
 
@@ -6,3 +10,6 @@ print(letras)  # {"b", "a", "c", "x", "i"}
 
 carros = set(("palio", "gol", "celta", "palio"))
 print(carros)  # {"gol", "celta", "palio"}
+
+linguagens = {"python", "java", "python"}
+print(linguagens)
