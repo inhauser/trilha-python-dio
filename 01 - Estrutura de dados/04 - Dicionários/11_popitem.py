ddf145942d56f6788b3d533ceb8f49dcb093918a
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-28 07:46:23 -0300

    Update 11_popitem.py

--- 01 - Estrutura de dados/04 - Dicionários/11_popitem.py
+++ 01 - Estrutura de dados/04 - Dicionários/11_popitem.py
@@ -1,3 +1,7 @@
+# Método
+# Difrença para o pop é que aqui não informa a chave, e vai retirando itens na sequência.
+# Se estiver vazio retorna erro / exceção.
+
 contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
 
 resultado = contatos.popitem()  # ('guilherme@gmail.com', {'nome': 'Guilherme', 'telefone': '3333-2221'})
