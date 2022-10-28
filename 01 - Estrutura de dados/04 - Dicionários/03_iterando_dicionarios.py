e286e62abc0f9f97188485afda48b7c9cba51eeb
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-28 07:31:31 -0300

    Update 03_iterando_dicionarios.py

--- 01 - Estrutura de dados/04 - Dicionários/03_iterando_dicionarios.py
+++ 01 - Estrutura de dados/04 - Dicionários/03_iterando_dicionarios.py
@@ -5,10 +5,10 @@ contatos = {
     "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
 }
 
-for chave in contatos:
+for chave in contatos: # não é a melhor opção de código, vide opção abaixo.
     print(chave, contatos[chave])
 
 print("=" * 100)
 
-for chave, valor in contatos.items():
+for chave, valor in contatos.items(): # melhor forma de escrever os comandos.
     print(chave, valor)
