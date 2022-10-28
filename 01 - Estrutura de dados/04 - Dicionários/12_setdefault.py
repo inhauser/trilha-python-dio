d2501fb211acdda5f7b2cb45dc803ee7f38c038b
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-28 07:49:38 -0300

    Update 12_setdefault.py

--- 01 - Estrutura de dados/04 - Dicionários/12_setdefault.py
+++ 01 - Estrutura de dados/04 - Dicionários/12_setdefault.py
@@ -1,3 +1,8 @@
+# Método
+# Muito utilizado no dia-a-dia
+# Se o atributo não estiver não estiver no dicionário, adiciona o atributo com o valor que inseri no código.
+# SE o atributo estiver no dicionário, ele retorna o valor do dicionário e não altera ele.
+
 contato = {"nome": "Guilherme", "telefone": "3333-2221"}
 
 contato.setdefault("nome", "Giovanna")  # "Guilherme"
