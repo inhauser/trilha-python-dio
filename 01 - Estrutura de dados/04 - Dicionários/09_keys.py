5b94bfbdfdbf0b144ed3c7531bd116abebb6ce2c
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-28 07:43:30 -0300

    Update 09_keys.py

--- 01 - Estrutura de dados/04 - Dicionários/09_keys.py
+++ 01 - Estrutura de dados/04 - Dicionários/09_keys.py
@@ -1,4 +1,11 @@
+# Método
+# Retorna somente as chaves de um dicionário
+# Útil para saber quais as chaves que o dicionário tem.
+
 contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
 
 resultado = contatos.keys()  # dict_keys(['guilherme@gmail.com'])
 print(resultado)
+
+novo_dicionario = {"a": 100, 1: "teste", "b": "python"}
+print(novo_dicionario.keys())
