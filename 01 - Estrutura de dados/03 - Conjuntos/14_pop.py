3720a3c200f06c6536f666a59c062218f539227e
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-27 21:26:34 -0300

    Update 14_pop.py

--- 01 - Estrutura de dados/03 - Conjuntos/14_pop.py
+++ 01 - Estrutura de dados/03 - Conjuntos/14_pop.py
@@ -1,3 +1,5 @@
+# no caso de set/conjunto, ele não retira a partir do último valor, mas do primeiro.
+
 numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
 
 print(numeros)  # {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
