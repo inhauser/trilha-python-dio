a6347521618013e4226970ceaf01c9afdabb8a31
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-28 07:50:47 -0300

    Update 12_setdefault.py

--- 01 - Estrutura de dados/04 - Dicionários/12_setdefault.py
+++ 01 - Estrutura de dados/04 - Dicionários/12_setdefault.py
@@ -2,6 +2,7 @@
 # Muito utilizado no dia-a-dia
 # Se o atributo não estiver não estiver no dicionário, adiciona o atributo com o valor que inseri no código.
 # SE o atributo estiver no dicionário, ele retorna o valor do dicionário e não altera ele.
+# Útil para o caso de se precisar um valor caso ele não exista, sem ter que fazer essa verificação antes. E preserva o valor se já existente, sem sobrescrever.
 
 contato = {"nome": "Guilherme", "telefone": "3333-2221"}
 
