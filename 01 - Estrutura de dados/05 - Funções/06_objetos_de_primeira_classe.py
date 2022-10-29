825344cac1f1d68928738ce8c1e483c6ea159181
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-29 10:56:35 -0300

    Update 06_objetos_de_primeira_classe.py

--- 01 - Estrutura de dados/05 - Funções/06_objetos_de_primeira_classe.py
+++ 01 - Estrutura de dados/05 - Funções/06_objetos_de_primeira_classe.py
@@ -5,6 +5,9 @@
 def somar(a, b):
     return a + b
 
+def subtrair(a,b)
+    return a - b
+
 
 def exibir_resultado(a, b, funcao):
     resultado = funcao(a, b)
@@ -12,3 +15,4 @@ def exibir_resultado(a, b, funcao):
 
 
 exibir_resultado(10, 10, somar)  # O resultado da operação 10 + 10 = 20
+exibir_resultado(10, 10, subtrair)  # O resultado da operação 10 - 10 = 0
