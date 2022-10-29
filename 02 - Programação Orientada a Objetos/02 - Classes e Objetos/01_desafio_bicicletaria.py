c459609e148fea5aa6f32c72a20456e7b0e4bdb6
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-29 18:55:31 -0300

    Update 01_desafio_bicicletaria.py

--- 02 - Programação Orientada a Objetos/02 - Classes e Objetos/01_desafio_bicicletaria.py
+++ 02 - Programação Orientada a Objetos/02 - Classes e Objetos/01_desafio_bicicletaria.py
@@ -3,19 +3,20 @@
 # Uma bicicleta pode: buzinar, parar e correr. 
 # Adicione esses comportamentos!
 
-# self é uma referência explícita para o objeto; queremos dizer que essa é a instância do objeto; poderia chamar de this, mas a convenção é chamar self.
+# self é uma referência explícita para o objeto; queremos dizer que essa é a instância do objeto; 
+# poderia chamar de this, mas a convenção é chamar self.
+
 
 class Bicicleta:
     def __init__(self, cor, modelo, ano, valor): # método construtor
-        # pass se quiser deixar em branco.
+        # pass - se quisesse deixar vazio
         self.cor = cor # atributos da classe
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
-        
-b1
 
-    def buzinar(self):
+    def buzinar(self): # método / comportamento. Métodos são funções dentro de uma classe.
+        # diferença em método é que deve passar um primeiro argumento que chama self.
         print("Plim plim...")
 
     def parar(self):
@@ -25,8 +26,13 @@ b1
     def correr(self):
         print("Vrummmmm...")
 
-    def __str__(self):
-        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+    #def get_cor(self):
+     #   return(self.cor)
+    # erro comum é esquecer o self, o argumento do método, conteha ele valor ou não.
+
+    def __str__(self): # método para retornar a exibição da instância de uma forma mais amigável (vide print (b2) abaixo)
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}" # dinâmico
+        #return f"Bicicleta: cor={self.cor}, modelo={self.modelo}, ano={self.ano}, valor={self.valor}" # hard coded.
 
 
 b1 = Bicicleta("vermelha", "caloi", 2022, 600) # objeto instanciado
@@ -36,5 +42,8 @@ b1.parar()
 print(b1.cor, b1.modelo, b1.ano, b1.valor)
 
 b2 = Bicicleta("verde", "monark", 2000, 189)
-print(b2)
+#b2.buzinar() # Bicicleta.buzinar(b2)
+print(b2) # exibir instância
 b2.correr()
+#b2.cor()
+#print(b2.get_cor())
