fbd9c9b3eda1ceba45273f16febc210ec258f0e2
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-29 10:57:32 -0300

    Update 06_objetos_de_primeira_classe.py

--- 01 - Estrutura de dados/05 - Funções/06_objetos_de_primeira_classe.py
+++ 01 - Estrutura de dados/05 - Funções/06_objetos_de_primeira_classe.py
@@ -11,7 +11,7 @@ def subtrair(a,b)
 
 def exibir_resultado(a, b, funcao):
     resultado = funcao(a, b)
-    print(f"O resultado da operação {a} + {b} = {resultado}")
+    print(f"O resultado da operação é = {resultado}")
 
 
 exibir_resultado(10, 10, somar)  # O resultado da operação 10 + 10 = 20
