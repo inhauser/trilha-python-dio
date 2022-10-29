85eefe0f85a21690de88035a5ae868ee10989eaa
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-29 10:22:30 -0300

    Update 04_parametros_somente_por_posicao copy.py

--- 01 - Estrutura de dados/05 - Funções/04_parametros_somente_por_posicao copy.py
+++ 01 - Estrutura de dados/05 - Funções/04_parametros_somente_por_posicao copy.py
@@ -1,6 +1,16 @@
-def criar_carro(modelo, ano, placa, /, marca, motor, combustivel):
+# Parâmetros podem ser passados por posição, por posição e nome, ou por nome.
+# def f(pos1, pos2, /, pos_or_kwd, *, kwd1, kwd2):
+#       positional only => positional or keyword => keyword only
+
+def criar_carro(modelo, ano, placa, /, marca, motor, combustivel): # antes de /, modelo ano e placa: somente por posição; após /, pode ser nome ou posição. 
     print(modelo, ano, placa, marca, motor, combustivel)
 
 
 criar_carro("Palio", 1999, "ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")
-criar_carro(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")  # inválido
+#válido, pois passa o três primeiros itens posicionados, e os três últimos nomeados.
+
+criar_carro("Palio", 1999, "ABC-1234", "Fiat", "1.0", "Gasolina")
+# também é válido, pois com a / na função, depois dos três primeiros itens por ser posição ou nomeado.
+
+#criar_carro(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")
+# inválido, pois tentou passar nomeado as três primeiras posições.
