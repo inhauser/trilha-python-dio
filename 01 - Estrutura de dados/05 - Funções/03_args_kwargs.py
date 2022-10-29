68327582b51082b6ef1e493d121eeea8c3899e2f
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-29 10:12:42 -0300

    Update 03_args_kwargs.py

--- 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
+++ 01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
@@ -1,12 +1,16 @@
+# Podemos combinar parâmetros obrigatórios com args e kwargs (nomes por convenção, mas podem ser outros que desejarmos.
+# Quando definidos, o método recebe os valores como tupla e dicionário respectivamente.
+
 def exibir_poema(data_extenso, *args, **kwargs):
     texto = "\n".join(args)
-    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
+    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()]) #.join para concatennar. .items por ser dicionário.
     mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
     print(mensagem)
 
 
 exibir_poema(
-    "Zen of Python",
+    "Sexta-feira, 26 de agosto de 2022", # primeiro item da função; considera data_extenso
+    "Zen of Python", # Args acima como texto - inserção como tuplas no padrão "", e enter sucessivamente até terminar args e começar kwargs (dicionário)
     "Beautiful is better than ugly.",
     "Explicit is better than implicit.",
     "Simple is better than complex.",
@@ -26,6 +30,7 @@ exibir_poema(
     "If the implementation is hard to explain, it's a bad idea.",
     "If the implementation is easy to explain, it may be a good idea.",
     "Namespaces are one honking great idea -- let's do more of those!",
-    autor="Tim Peters",
+    autor="Tim Peters", # quando termina a inbserção por tuplas "", , reconhece que não é mais args. Iniciados kwargs, aciam como meta_dados, 
+    #e fazemos a inserção chave:valor do dicionário.
     ano=1999,
 )
