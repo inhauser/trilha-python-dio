d77139f175ecaed77a121075aab2b6948f994c3f
inhauser <84652335+inhauser@users.noreply.github.com>
2022-10-29 10:32:30 -0300

    Update 05_parametros_somente_por_nome.py

--- 01 - Estrutura de dados/05 - Funções/05_parametros_somente_por_nome.py
+++ 01 - Estrutura de dados/05 - Funções/05_parametros_somente_por_nome.py
@@ -3,9 +3,10 @@
 #       positional only => positional or keyword => keyword only
 # Neste caso, exploraremos apenas a keyword only, "nomeados", excluindo as duas primeiras.
 
-def criar_carro(modelo, ano, placa, /, *, marca, motor, combustivel):
+def criar_carro(*, modelo, ano, placa, marca, motor, combustivel):
     print(modelo, ano, placa, marca, motor, combustivel)
 
+criar_carro(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")  # válido
 
 # criar_carro("Palio", 1999, "ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")
-criar_carro(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")  # inválido
+# inválido, pois somente pode admitir nomeado, em virtude do *, na função; e os 3 primeiro itens estão apenas com posição.
