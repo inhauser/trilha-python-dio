6dfeed9ebfbe9049c4e6e62dd1f874cf41f551f2
inhauser <84652335+inhauser@users.noreply.github.com>
2022-11-01 20:40:19 -0300

    Update 03_propriedades_exemplo_pessoa.py

--- 02 - Programação Orientada a Objetos/05 - Encapsulamento/03_propriedades_exemplo_pessoa.py
+++ 02 - Programação Orientada a Objetos/05 - Encapsulamento/03_propriedades_exemplo_pessoa.py
@@ -4,10 +4,20 @@ class Pessoa:
         self._ano_nascimento = ano_nascimento
 
     @property
+    def nome(self):
+        return self._nome
+
+    @property
     def idade(self):
         _ano_atual = 2022
         return _ano_atual - self._ano_nascimento
 
+    # def get_nome(self): mais comum em Java e C++
+        return self._nome
+    
+    # def get_idade(self): mais comum em Java e C++
+        return 2022 - self._ano_nascimento
 
 pessoa = Pessoa("Guilherme", 1994)
 print(f"Nome: {pessoa.nome} \tIdade: {pessoa.idade}")
+# print(f"NOme: {pessoa.get_nome()} \tIdade: {pessoa.get_idade()}")
