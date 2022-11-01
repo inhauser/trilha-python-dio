3707df6804d8ffe8c592fca239cfe45760c7d7a8
inhauser <84652335+inhauser@users.noreply.github.com>
2022-11-01 20:12:04 -0300

    Update 01_encapsulamento.py

--- 02 - Programação Orientada a Objetos/05 - Encapsulamento/01_encapsulamento.py
+++ 02 - Programação Orientada a Objetos/05 - Encapsulamento/01_encapsulamento.py
@@ -1,3 +1,16 @@
+#O encapsulamento é um dos conceitos fundamentais em programação orientada a objetos. 
+# Ele descreve a ideia de agrupar dados e os métodos que manipulam esses dados em uma unidade. 
+# Isso impõe restrições ao acesso direto a variáveis ​​e métodos e pode evitar a modificação acidental de dados. 
+# Para evitar alterações acidentais, a variável de um objeto só pode ser alterada pelo método desse objeto.
+# em diagrama sinal de - informa que é privado - só pode ser acessado pela classe: ex. saldo.
+# em diagrama sinal de + informa que é público - pode ser acessado fora da classe: ex: cliente depositar e sacar.
+# Convenções nome: _nome é privado.
+# Todos os recursos são públicos, a menos que o nome inicie com underline. 
+# Ou seja, o interpretador Python não irá garantir a proteção do recurso, mas por ser uma convenção amplamente adotada na comunidade, 
+# quando encontramos uma variável e/ou método com nome iniciado por underline, sabemos que não deveríamos manipular o seu valor diretamente, 
+# ou invocar o método fora do escopo da classe.
+
+
 class Conta:
     def __init__(self, nro_agencia, saldo=0):
         self._saldo = saldo
