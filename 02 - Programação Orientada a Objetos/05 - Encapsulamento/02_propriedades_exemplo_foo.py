b5af1ee23ef93e6f994f47add214bad7bcde0fbd
inhauser <84652335+inhauser@users.noreply.github.com>
2022-11-01 20:30:20 -0300

    Update 02_propriedades_exemplo_foo.py

--- 02 - Programação Orientada a Objetos/05 - Encapsulamento/02_propriedades_exemplo_foo.py
+++ 02 - Programação Orientada a Objetos/05 - Encapsulamento/02_propriedades_exemplo_foo.py
@@ -1,3 +1,11 @@
+# PROPERTIES
+# Com o property() do Python, você pode criar atributos gerenciados em suas classes. 
+# Você pode usar atributos gerenciados, também conhecidos como propriedades, 
+# quando precisar modificar sua implementação interna sem alterar a API pública da classe.
+# @property : um decorador, que é uma função que  executa antes da função.
+
+
+
 class Foo:
     def __init__(self, x=None):
         self._x = x
@@ -6,11 +14,11 @@ class Foo:
     def x(self):
         return self._x or 0
 
-    @x.setter
+    @x.setter # modificação - para poder modificar o x
     def x(self, value):
         self._x += value
 
-    @x.deleter
+    @x.deleter # deleção
     def x(self):
         self._x = 0
 
