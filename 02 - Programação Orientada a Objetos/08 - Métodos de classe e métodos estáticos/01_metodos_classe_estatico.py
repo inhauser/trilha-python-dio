31bb430845feff81c6b4ea9ecac09648c1a5356a
inhauser <84652335+inhauser@users.noreply.github.com>
2022-11-02 11:23:24 -0300

    Update 01_metodos_classe_estatico.py

--- 02 - Programação Orientada a Objetos/08 - Métodos de classe e métodos estáticos/01_metodos_classe_estatico.py
+++ 02 - Programação Orientada a Objetos/08 - Métodos de classe e métodos estáticos/01_metodos_classe_estatico.py
@@ -1,3 +1,21 @@
+# MÉTODOS DE CLASSE
+#Métodos de classe estão ligados à classe e não ao objeto. 
+# Eles têm acesso ao estado da classe, pois recebem um parâmetro que aponta para a classe e não para a instância do objeto.
+
+# MÉTODOS ESTÁTICOS
+# Um método estático não recebe um primeiro argumento explícito. 
+# Ele também é um método vinculado à classe e não ao objeto da classe. 
+# Este método não pode acessar ou modificar o estado da classe. 
+# Ele está presente em uma classe porque faz sentido que o método esteja presente na classe.
+
+# MÉTODOS DE CLASSE X MÉTODOS ESTÁTICOS
+# Um método de classe recebe um primeiro parâmetro que aponta para a classe, enquanto um método estático não.
+# Um método de classe pode acessar ou modificar o estado da classe enquanto um método estático não pode acessá-lo ou modificá-lo.
+
+# Quando utilizar método de classe ou estático
+# Geralmente usamos o método de classe para criar métodos de fábrica, que são métodos que retornam instâncias da classe.
+# Geralmente usamos métodos estáticos para criar funções utilitárias. Exemplo, dentro da classe pessoa, para validar se é maior de idade.
+
 class Pessoa:
     def __init__(self, nome, idade):
         self.nome = nome
