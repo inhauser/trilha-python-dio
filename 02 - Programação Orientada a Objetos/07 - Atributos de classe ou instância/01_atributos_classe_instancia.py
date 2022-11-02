379a4a19e691e3bfc725b21d17ecd7343f1a65ab
inhauser <84652335+inhauser@users.noreply.github.com>
2022-11-02 11:03:28 -0300

    Update 01_atributos_classe_instancia.py

--- 02 - Programação Orientada a Objetos/07 - Atributos de classe ou instância/01_atributos_classe_instancia.py
+++ 02 - Programação Orientada a Objetos/07 - Atributos de classe ou instância/01_atributos_classe_instancia.py
@@ -1,3 +1,8 @@
+# ATRIBUTOS DO OBJETO
+# Todos os objetos nascem com o mesmo número de atributos de classe e de instância. 
+# Atributos de instância são diferentes para cada objeto (cada objeto tem uma cópia), 
+# já os atributos de classe são compartilhados entre os objetos.
+
 class Estudante:
     escola = "DIO"
 
@@ -18,6 +23,7 @@ aluno_1 = Estudante("Guilherme", 1)
 aluno_2 = Estudante("Giovanna", 2)
 mostrar_valores(aluno_1, aluno_2)
 
-Estudante.escola = "Python"
-aluno_3 = Estudante("Chappie", 3)
+Estudante.escola = "Python" # quando altero instância ela afeta todos os objetos.
+aluno_1 = Estudante("Guilherme", 4) # quando altero objeto os outros objetos não são afetados.
+aluno_3 = Estudante("Chappie", 3) 
 mostrar_valores(aluno_1, aluno_2, aluno_3)
